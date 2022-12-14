49070fad3bb1cbd10231de9936f33d9c3164481d
StayAWSM <alex.babichev.95@gmail.com>
2022-12-14 19:57:13 +0300

    Дополнили материал для ДЗ

--- Re-tasks.py
+++ Re-tasks.py
@@ -23,3 +23,49 @@
 #     res += ar[n]
 # print(ar)
 # print('Сумма элементов списка равна', res)
+
+
+'''Задачи к ДЗ'''
+# # Задача 1. Вводится список целых чисел в одну строчку через пробел.
+# # Необходимо оставить в нем только двузначные числа.
+# # Реализовать программу с использованием функции filter.
+# # Результат отобразить на экране в виде последовательности
+# # оставшихся чисел в одну строчку через пробел.
+# # пример - 8 11 0 -23 140 1 => 11 -23
+#
+# list_num = list(map(str, list(filter(lambda x: 10 <= abs(x) <100, map(int, \
+#             list(input('Введите список целых чисел через пробел: ').split())) ))))
+# print(" ".join(list_num))
+
+
+
+# # Задача 2. Дан список, вывести отдельно буквы и цифры.
+# # a = ( "a", 'b', '2', '3' ,'c')
+# # b = ( 'a' , 'b' , 'c')
+# # c = ( '2', '3')
+#
+# start_list = ['a', 'b', '2', '3', 'c']
+# print('a =', start_list)
+# print('b =', list(filter(lambda x: not x.isdigit(), start_list)))
+# print('c =', list(filter(lambda x: x.isdigit(), start_list)))
+
+
+
+# # Задача 3. Преобразовать набора списков
+# # users = ['user1','user2','user3','user4','user5']
+# # ids = [4, 5, 9, 14, 7] salary = [111,222,333] в другой набор
+# # ['user1', 4, 111]
+# # ['user2', 5, 222]
+# # ['user3', 9, 333]
+# # и потом вернуть в исходное состояние
+# # ['user1', 'user2', 'user3']
+# # [4, 5, 9]
+# # [111, 222, 333]
+#
+# users = ['user1','user2','user3','user4','user5']
+# ids = [4, 5, 9, 14, 7]
+# salary = [111,222,333]
+# rar = list(zip(users, ids, salary))
+# print(rar)
+# users, ids, salary = zip(*rar)
+# print(f'users = {users}\nids = {ids}\nsalary = {salary}')
